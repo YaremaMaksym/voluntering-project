e9c32a390af7225fd99043c427a25785d571e79c
gfton <gftonot@gmail.com>
2023-05-27 19:56:42 +0300

    Fixed login, to check password

--- server/src/main/java/com/ftf/volunteeringproject/login/LoginService.java
+++ server/src/main/java/com/ftf/volunteeringproject/login/LoginService.java
@@ -6,6 +6,7 @@ import com.ftf.volunteeringproject.volunteer.VolunteerRepository;
 import com.ftf.volunteeringproject.volunteer.VolunteerService;
 import com.ftf.volunteeringproject.volunteer.VolunteerUserDetails;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -17,14 +18,11 @@ import java.util.Optional;
 @AllArgsConstructor
 public class LoginService {
     private final JwtService jwtService;
-    private final VolunteerRepository volunteerRepository;
+    private final AuthenticationManager authenticationManager;
 
     public String login(LoginDto loginDto) {
-        Optional<Volunteer> volunteer = volunteerRepository.findByEmail(loginDto.email());
-
-        if (volunteer.isEmpty()) {
-            throw new RuntimeException("User not found");
-        }
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.email(), loginDto.password()));
 
         UserDetails userDetails = new VolunteerUserDetails(loginDto.email(), loginDto.password());
         return jwtService.generateJwt(new HashMap<>(), userDetails);
