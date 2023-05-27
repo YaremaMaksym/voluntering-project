ae5bf2588ffc370b957ddbb6b6ac574b1240dbbb
gfton <gftonot@gmail.com>
2023-05-27 16:11:00 +0300

    Init

---
+++ server/src/main/java/com/ftf/volunteeringproject/VolunteeringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ftf.volunteeringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteeringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteeringProjectApplication.class, args);
+	}
+
+}
